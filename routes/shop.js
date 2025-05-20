d670c05e2627fdbd7bdd31557d30a802e51708e3
Vickykumarverma-11 <vermavickykumar25@gmail.com>
2025-05-20 23:44:08 +0530

    dynamic route

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,12 @@ const shopController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
+
 router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;
