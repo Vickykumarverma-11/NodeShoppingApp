798041c1372c6c6b9416c653fb81661fcc3094df
Vickykumarverma-11 <vermavickykumar25@gmail.com>
2025-05-25 23:35:32 +0530

    added mongo db

--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', '12345678', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongiClient = mongodb.MongoClient;
+let _db;
+const mongoConnect = callback => {
+  MongiClient.connect('mongodb+srv://bickyvermamsp420:Bicky123@cluster0.shiyqdv.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0').
+    then(client => {
+      console.log('CONNECTED');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
-module.exports = sequelize;
